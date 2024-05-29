fe88798e22d16fa432595c81b9c748f3b899fbb0
landrynkayou <eng.landrynkayou@gmail.com>
2024-05-29 18:26:41 +0200

    sixth commit

--- app/src/main/java/com/example/bikesalesapplication/mainactivity.java
+++ app/src/main/java/com/example/bikesalesapplication/mainactivity.java
@@ -1,4 +1,33 @@
 package com.example.bikesalesapplication;
 
-public class mainactivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+
+    // Method to navigate to BikeItemActivity
+    public void goToBikeItemActivity(View view) {
+        Intent intent = new Intent(this, Bike.class);
+        startActivity(intent);
+    }
+
+    // Method to navigate to ViewBikeActivity
+    public void goToViewBikeActivity(View view) {
+        Intent intent = new Intent(this, ViewBikeActivity.class);
+        startActivity(intent);
+    }
+
+    // Method to navigate to SearchActivity
+    public void goToSearchActivity(View view) {
+        Intent intent = new Intent(this, SearchBikeActivity.class);
+        startActivity(intent);
+    }
 }
